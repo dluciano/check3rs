c9b364284591278e3bd0d11a9812c05375bcd287
Dawlin <dawmusic19@gmail.com>
2024-09-10 04:28:35 +0100

    test new ai params

--- src/lib/checker/getStatsForMoveResult.ts
+++ src/lib/checker/getStatsForMoveResult.ts
@@ -13,8 +13,8 @@ export const getStatsForMoveResult = (
   return (
     (isMenForwardStep ? 2 : 1) +
     numberOfCapturedMen * 2.3 +
-    numberOfCapturedKings * 5.5 +
-    (promotedToKing ? 20 : 0) +
-    (keepCapturing ? 13 : 0)
+    numberOfCapturedKings * 5.3 +
+    (promotedToKing ? 30 : 0) +
+    (keepCapturing ? 30 : 0)
   );
 };
